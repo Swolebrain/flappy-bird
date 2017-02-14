0246734dcafcc3a4605701a6f782c2bc7e5b2f64
Swolebrain <thecodingteacher@gmail.com>
2017-02-14 00:19:34 -0500

    feat: finished v2

--- js/Bird.js
+++ js/Bird.js
@@ -7,25 +7,66 @@ const Bird = function(x, y, ctx){
   this.height = 64;
   this.ticks = 0;
   this.spriteIndex = 0;
+  this.dead = false;
   this.sprites = [document.getElementById('bird1'),
         document.getElementById('bird2'),
         document.getElementById('bird3')];
   var self = this;
   window.addEventListener('keydown', function(e){
-    if (e.keyCode === 32){
+    if (e.keyCode === 32 && !self.dead){
       self.velY = -16;
     }
   });
 };
-Bird.prototype.update = function(){
-  this.ticks++;
-  if (this.ticks % 15 === 0) this.spriteIndex = (this.spriteIndex+1) % this.sprites.length;
+Bird.prototype.update = function(pipes){
   this.y += this.velY;
   this.velY += 1.25;
+  if (this.detectCollisions(pipes)){
+    this.dead = true;
+  }
+
+  this.ticks++;
+  if (this.ticks % 15 === 0) this.spriteIndex = (this.spriteIndex+1) % this.sprites.length;
+
 };
 
 Bird.prototype.render = function(){
-  let renderX = this.x - this.width/2;
-  let renderY = this.y - this.height/2;
+  let renderX = - this.width/2;
+  let renderY = - this.height/2;
+  this.ctx.save();
+  this.ctx.translate(this.x, this.y);
+  let angle = Math.PI/6 * this.velY/16;
+  this.ctx.rotate(angle);
   this.ctx.drawImage(this.sprites[this.spriteIndex], renderX, renderY);
+
+  this.ctx.restore();
+};
+
+Bird.prototype.detectCollisions = function(pipes){
+  for(var i = 0; i < pipes.length; i++){
+    let e = pipes[i];
+    let highPipe = e.ypos <= 0;
+    let x0 = e.xpos, x1 = e.xpos+e.width;
+    let alpha2 = this.x + 44;
+    let beta2 = this.y;
+    if (highPipe ){
+      let y0 = e.ypos + e.length;
+      let alpha = this.x;
+      let beta = this.y - this.height/2;
+      if (alpha > x0 && alpha < x1 && beta < y0 ||
+          alpha2 > x0 && alpha2 < x1 && beta2 < y0){
+        return true;
+      }
+    }
+    else{
+      let y2 = e.ypos;
+      let a = this.x;
+      let b = this.y + this.height/2;
+      if (a > x0 && a < x1 && b > y2 ||
+          alpha2 > x0 && alpha2 < x1 && beta2 > y2) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
--- js/main.js
+++ js/main.js
@@ -9,10 +9,12 @@
    const environment = new Environment(c, ctx);
    const bird = new Bird(250, 300, ctx);
    const pipes = [];
+   let pipeSet = generateRandomPipes(ctx, c.width, c.height);
+   pipes.push(pipeSet.top, pipeSet.bottom);
    setInterval(function(){
      let pipeSet = generateRandomPipes(ctx, c.width, c.height);
      pipes.push(pipeSet.top, pipeSet.bottom);
-   }, 3000);
+   }, 2600);
    gameLoop();
 
 
@@ -20,18 +22,22 @@
     MAIN GAME LOOP
    */
    function gameLoop(){
-     ctx.fillRect(0,0,c.width,c.height);
-     environment.update();
+     //ctx.fillRect(0,0,c.width,c.height);
+     bird.update(pipes);
+     if (!bird.dead){
+       environment.update();
+
+       pipes.forEach(function(pipe1){
+         pipe1.update();
+       });
+     }
      environment.render();
      pipes.forEach(function(pipe1){
-       pipe1.update();
        pipe1.render();
      });
-     bird.update();
      bird.render();
-     if (detectCollisions(bird, pipes)) {
-       alert("you lose!");
-       window.location = '/';
+     if (bird.dead){
+       drawGameOver(ctx, c);
      }
      window.requestAnimationFrame(gameLoop);
    }
@@ -46,25 +52,9 @@
    return returnVal;
  }
 
- function detectCollisions(bird, pipes){
-   for(var i = 0; i < pipes.length; i++){
-     let e = pipes[i];
-     let highPipe = e.ypos <= 0;
-     let x0 = e.xpos, x1 = e.xpos+e.width;
-     if (highPipe ){
-       let y0 = e.ypos + e.length;
-       let alpha = bird.x;
-       let beta = bird.y - bird.height/2;
-       if (alpha > x0 && alpha < x1 && beta < y0){
-         return true;
-       }
-     }
-     else{
-       let y2 = e.ypos;
-       let a = bird.x;
-       let b = bird.y + bird.height/2;
-       if (a > x0 && a < x1 && b > y2) return true;
-     }
-   }
-   return false;
- }
+
+function drawGameOver(ctx, c){
+  ctx.font="30px Verdana";
+  ctx.textAlign="center";
+  ctx.fillText("Game Over!!",c.width/2 , c.height/2);
+}
