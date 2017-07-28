e15d19c4f32c3262e2b902707a4931a1d16f2589
swolebrain <thecodingteacher@gmail.com>
2017-07-27 21:27:41 -0400

    Feat: removed scrolling

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
  window.onload = function(){
    const c = document.getElementById('canvas');
    c.width = window.innerWidth;
-   c.height = 600;
+   c.height = window.innerHeight;
 
    const ctx = c.getContext('2d');
 
